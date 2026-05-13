e26f4249c2f22e9ad87e2c1505201297e79997f7
{오지우} <ojing0116@skuniv.ac.kr>
2026-05-13 10:50:23 +0900

    :fire: Remove: remove migration db

--- src/main/resources/db/migration/V3__alter_campaign.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE campaign
-    ADD COLUMN IF NOT EXISTS ad_id              BIGINT,
-    ADD COLUMN IF NOT EXISTS coupon_id          BIGINT,
-    ADD COLUMN IF NOT EXISTS batch_cycle        VARCHAR(20),
-    ADD COLUMN IF NOT EXISTS batch_time         TIME,
-    ADD COLUMN IF NOT EXISTS batch_day_of_week  VARCHAR(10),
-    ADD COLUMN IF NOT EXISTS batch_day_of_month INTEGER;
